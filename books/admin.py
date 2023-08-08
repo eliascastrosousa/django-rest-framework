4e3d8ceee7a1ac5658ef5180d353f1ce1fd3441c
eliascastrosousa <73971067+eliascastrosousa@users.noreply.github.com>
2023-08-08 20:23:39 -0300

    commit

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from books import models
+
+admin.site.register(models.Books)
 
 # Register your models here.
--- books/models.py
+++ books/models.py
@@ -10,3 +10,6 @@ class Books(models.Model):
     pages = models.IntegerField()
     publishing_company = models.CharField(max_length=255)
     create_at = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
